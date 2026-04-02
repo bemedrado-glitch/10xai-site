e7407ed0ab33267622a0f69a60a89a31b5030d2c
Bernardo Medrado <bmedrado@internal.salesforce.com>
2026-04-01 20:58:13 -0400

    chore: prepare launch SEO assets

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://10xai.us/sitemap.xml",
+    host: "https://10xai.us",
+  };
+}
